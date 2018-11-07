3890d0ea11e1de581c7de65df1fab35dfb9f7068
moritz.huebner <email@moritz-huebner.de>
2018-11-07 13:50:28 +1100

    Added some tests, did some refactoring, and wrote some docstrings

--- bilby/core/sampler/polychord.py
+++ bilby/core/sampler/polychord.py
@@ -1,16 +1,34 @@
 from __future__ import absolute_import
 
 import numpy as np
-import PyPolyChord
-from PyPolyChord.settings import PolyChordSettings
 
 from .base_sampler import NestedSampler
 from ..utils import logger
 
-# from PyPolyChord.priors import UniformPrior
+try:
+    import PyPolyChord
+    from PyPolyChord.settings import PolyChordSettings
+except ImportError:
+    logger.debug("PyPolyChord is not installed on this system, you will not"
+                 "be able to use the PolyChord Sampler")
 
 
 class Polychord(NestedSampler):
+
+    """
+    Bilby wrapper of PyPolyChord
+    https://arxiv.org/abs/1506.00171
+
+    The code is currently only available on, although the author
+    announced to move it to github soon(TM)
+    https://ccpforge.cse.rl.ac.uk/gf/project/polychord/
+
+    Please contact us if you can't access this code.
+
+    Keyword arguments will be passed into `PyPolyChord.run_polychord` into the `settings`
+    argument. See the PolyChord documentation for what all of those mean.
+    """
+
     default_kwargs = dict(use_polychord_defaults=False, nlive=None, num_repeats=None,
                           nprior=-1, do_clustering=True, feedback=1, precision_criterion=0.001,
                           logzero=-1e30, max_ndead=-1, boost_posterior=0.0, posteriors=True,
@@ -21,6 +39,9 @@ class Polychord(NestedSampler):
                           file_root='test', seed=-1, grade_dims=None, grade_frac=None, nlives={})
 
     def run_sampler(self):
+        import PyPolyChord
+        from PyPolyChord.settings import PolyChordSettings
+
         if self.kwargs['use_polychord_defaults']:
             settings = PolyChordSettings(nDims=self.ndim, nDerived=self.ndim)
         else:
@@ -28,14 +49,15 @@ class Polychord(NestedSampler):
             pc_kwargs = self.kwargs.copy()
             pc_kwargs.pop('use_polychord_defaults')
             settings = PolyChordSettings(nDims=self.ndim, nDerived=self.ndim, **pc_kwargs)
-
         self._verify_kwargs_against_default_kwargs()
-        PyPolyChord.run_polychord(loglikelihood=self.log_likelihood_wrapper, nDims=self.ndim,
+
+        PyPolyChord.run_polychord(loglikelihood=self.log_likelihood, nDims=self.ndim,
                                   nDerived=self.ndim, settings=settings, prior=self.prior_transform)
 
         return None
 
     def _setup_dynamic_defaults(self):
+        """ Sets up some interdependent default argument if none are given by the user """
         if not self.kwargs['grade_dims']:
             self.kwargs['grade_dims'] = [self.ndim]
         if not self.kwargs['grade_frac']:
@@ -43,24 +65,14 @@ class Polychord(NestedSampler):
         if not self.kwargs['nlive']:
             self.kwargs['nlive'] = self.ndim * 25
         if not self.kwargs['num_repeats']:
-            self.kwargs['num_repeats'] = self.ndim * 25
+            self.kwargs['num_repeats'] = self.ndim * 5
 
-    def _verify_kwargs_against_default_kwargs(self):
-        """
-        Check if the kwargs are contained in the list of available arguments
-        of the external sampler.
-        """
-        args = self.default_kwargs
+    def _translate_kwargs(self, kwargs):
+        if 'nlive' not in kwargs:
+            for equiv in self.npoints_equiv_kwargs:
+                if equiv in kwargs:
+                    kwargs['nlive'] = kwargs.pop(equiv)
 
-        bad_keys = []
-        for user_input in self.kwargs.keys():
-            if user_input not in args:
-                logger.warning(
-                    "Supplied argument '{}' not an argument of '{}', removing."
-                    .format(user_input, self.__class__.__name__))
-                bad_keys.append(user_input)
-        for key in bad_keys:
-            self.kwargs.pop(key)
-
-    def log_likelihood_wrapper(self, theta):
-        return self.log_likelihood(theta), theta
+    def log_likelihood(self, theta):
+        """ Overrides the log_likelihood so that PolyChord understands it """
+        return super(Polychord, self).log_likelihood(theta), theta
--- test/sampler_test.py
+++ test/sampler_test.py
@@ -247,6 +247,50 @@ class TestNestle(unittest.TestCase):
             self.assertDictEqual(expected, self.sampler.kwargs)
 
 
+class TestPolyChord(unittest.TestCase):
+
+    def setUp(self):
+        self.likelihood = MagicMock()
+        self.priors = dict(a=bilby.prior.Uniform(0, 1))
+        self.sampler = bilby.core.sampler.Polychord(self.likelihood, self.priors,
+                                                    outdir='outdir', label='label',
+                                                    use_ratio=False, plot=False,
+                                                    skip_import_verification=True)
+
+    def tearDown(self):
+        del self.likelihood
+        del self.priors
+        del self.sampler
+
+    def test_default_kwargs(self):
+        expected = dict(use_polychord_defaults=False, nlive=self.sampler.ndim*25, num_repeats=self.sampler.ndim*5,
+                        nprior=-1, do_clustering=True, feedback=1, precision_criterion=0.001,
+                        logzero=-1e30, max_ndead=-1, boost_posterior=0.0, posteriors=True,
+                        equals=True, cluster_posteriors=True, write_resume=True,
+                        write_paramnames=False, read_resume=True, write_stats=True,
+                        write_live=True, write_dead=True, write_prior=True,
+                        compression_factor=np.exp(-1), base_dir='polychord_chains',
+                        file_root='test', seed=-1, grade_dims=list([self.sampler.ndim]),
+                        grade_frac=list([1.0]*len([self.sampler.ndim])), nlives={})
+        self.assertDictEqual(expected, self.sampler.kwargs)
+
+    def test_translate_kwargs(self):
+        expected = dict(use_polychord_defaults=False, nlive=123, num_repeats=None,
+                        nprior=-1, do_clustering=True, feedback=1, precision_criterion=0.001,
+                        logzero=-1e30, max_ndead=-1, boost_posterior=0.0, posteriors=True,
+                        equals=True, cluster_posteriors=True, write_resume=True,
+                        write_paramnames=False, read_resume=True, write_stats=True,
+                        write_live=True, write_dead=True, write_prior=True,
+                        compression_factor=np.exp(-1), base_dir='polychord_chains',
+                        file_root='test', seed=-1, grade_dims=None, grade_frac=None, nlives={})
+        for equiv in bilby.core.sampler.base_sampler.NestedSampler.npoints_equiv_kwargs:
+            new_kwargs = self.sampler.kwargs.copy()
+            del new_kwargs['nlive']
+            new_kwargs[equiv] = 123
+            self.sampler.kwargs = new_kwargs
+            self.assertDictEqual(expected, self.sampler.kwargs)
+
+
 class TestPTEmcee(unittest.TestCase):
 
     def setUp(self):
