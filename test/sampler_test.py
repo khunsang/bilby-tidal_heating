2717a9e6e60b89c64d8f45b56fd3d7a621bc9843
Moritz <email@moritz-huebner.de>
2018-11-14 16:46:48 +1100

    Fixed tests

--- test/sampler_test.py
+++ test/sampler_test.py
@@ -272,17 +272,20 @@ class TestPolyChord(unittest.TestCase):
                         compression_factor=np.exp(-1), base_dir='outdir',
                         file_root='polychord', seed=-1, grade_dims=list([self.sampler.ndim]),
                         grade_frac=list([1.0]*len([self.sampler.ndim])), nlives={})
+        self.sampler._setup_dynamic_defaults()
         self.assertDictEqual(expected, self.sampler.kwargs)
 
     def test_translate_kwargs(self):
-        expected = dict(use_polychord_defaults=False, nlive=123, num_repeats=None,
+        expected = dict(use_polychord_defaults=False, nlive=123, num_repeats=self.sampler.ndim*5,
                         nprior=-1, do_clustering=True, feedback=1, precision_criterion=0.001,
                         logzero=-1e30, max_ndead=-1, boost_posterior=0.0, posteriors=True,
                         equals=True, cluster_posteriors=True, write_resume=True,
                         write_paramnames=False, read_resume=True, write_stats=True,
                         write_live=True, write_dead=True, write_prior=True,
                         compression_factor=np.exp(-1), base_dir='outdir',
-                        file_root='polychord', seed=-1, grade_dims=None, grade_frac=None, nlives={})
+                        file_root='polychord', seed=-1, grade_dims=list([self.sampler.ndim]),
+                        grade_frac=list([1.0]*len([self.sampler.ndim])), nlives={})
+        self.sampler._setup_dynamic_defaults()
         for equiv in bilby.core.sampler.base_sampler.NestedSampler.npoints_equiv_kwargs:
             new_kwargs = self.sampler.kwargs.copy()
             del new_kwargs['nlive']
@@ -466,6 +469,11 @@ class TestRunningSamplers(unittest.TestCase):
         _ = bilby.run_sampler(
             likelihood=self.likelihood, priors=self.priors, sampler='nestle',
             nlive=100, save=False)
+
+    def test_run_pypolychord(self):
+        _ = bilby.run_sampler(
+            likelihood=self.likelihood, priors=self.priors,
+            sampler='pypolychord', nlive=100, save=False)
 
     def test_run_ptemcee(self):
         _ = bilby.run_sampler(
