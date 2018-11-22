5ac98f056da888626811ad4d894a0505504433aa
Moritz <email@moritz-huebner.de>
2018-11-22 11:42:34 +1100

    Removed PolyChord from Python 2 tests

--- test/sampler_test.py
+++ test/sampler_test.py
@@ -475,7 +475,7 @@ class TestRunningSamplers(unittest.TestCase):
             nlive=100, save=False)
 
     def test_run_pypolychord(self):
-        if not sys.version_info < 3:
+        if sys.version_info[0] == 3:
             _ = bilby.run_sampler(
                 likelihood=self.likelihood, priors=self.priors,
                 sampler='pypolychord', nlive=100, save=False)
