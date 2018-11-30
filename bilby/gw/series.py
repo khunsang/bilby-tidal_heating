77fdaaf70d4002df445e523acccc414503d72a3d
Moritz <email@moritz-huebner.de>
2018-11-30 14:29:26 +1100

    Added a check for having sampling frequency and duration multiply to an integer

--- bilby/gw/series.py
+++ bilby/gw/series.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from ..core import utils
 
 
@@ -15,8 +17,9 @@ class CoupledTimeAndFrequencySeries(object):
     start_time: float, optional
         Starting time of the time array
         """
-        self.duration = duration
-        self.sampling_frequency = sampling_frequency
+        self._duration = duration
+        self._sampling_frequency = sampling_frequency
+        self._check_legal_duration_and_sampling_frequency()
         self.start_time = start_time
         self._frequency_array_updated = False
         self._time_array_updated = False
@@ -99,6 +102,7 @@ class CoupledTimeAndFrequencySeries(object):
     @duration.setter
     def duration(self, duration):
         self._duration = duration
+        self._check_legal_duration_and_sampling_frequency()
         self._frequency_array_updated = False
         self._time_array_updated = False
 
@@ -116,8 +120,25 @@ class CoupledTimeAndFrequencySeries(object):
     @sampling_frequency.setter
     def sampling_frequency(self, sampling_frequency):
         self._sampling_frequency = sampling_frequency
+        self._check_legal_duration_and_sampling_frequency()
         self._frequency_array_updated = False
         self._time_array_updated = False
+
+    def _check_legal_duration_and_sampling_frequency(self):
+        if self._sampling_frequency is None or self._duration is None:
+            return
+        num = self._sampling_frequency * self._duration
+        tol = 1e-8
+        if np.abs(num - np.round(num)) > tol:
+            raise IllegalDurationAndSamplingFrequencyException(
+                '\nYour sampling frequency and duration must multiply to a number'
+                'close to (tol = {}) an integer number. \nBut sampling_frequency={} and '
+                'duration={} multiply to {}'.format(
+                    tol, self._sampling_frequency, self._duration,
+                    self._sampling_frequency*self._duration
+                )
+            )
+        self._duration = np.round(self._sampling_frequency * self._duration) / self.sampling_frequency
 
     @property
     def start_time(self):
@@ -127,3 +148,7 @@ class CoupledTimeAndFrequencySeries(object):
     def start_time(self, start_time):
         self._start_time = start_time
         self._time_array_updated = False
+
+
+class IllegalDurationAndSamplingFrequencyException(Exception):
+    pass
