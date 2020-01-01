ce15a98d1e10a06d6a0ca31e81bc3cfe040caa54
Khun Sang Phukon <khun.phukon@ligo.org>
2020-01-01 07:05:10 -0800

    while space removed

--- bilby/gw/source.py
+++ bilby/gw/source.py
@@ -269,7 +269,7 @@ def lal_binary_neutron_star(
         a_1=a_1, a_2=a_2, tilt_1=tilt_1, tilt_2=tilt_2, phi_12=phi_12,
         phi_jl=phi_jl, lambda_1=lambda_1, lambda_2=lambda_2, **waveform_kwargs)
 
-    
+
 def lal_binary_bbh_bns_horizon(
         frequency_array, mass_1, mass_2, luminosity_distance, a_1, tilt_1,
         phi_12, a_2, tilt_2, phi_jl, theta_jn, phase, lambda_1, lambda_2,
